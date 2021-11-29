2b7090e8d912024beeaf4f428c6dd46a4c57e0ac
Fontinhas <joao.fontinhas@eyenov.com>
2021-11-29 10:14:31 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WrappingHStack",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15)
-    ],
+    platforms: [],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
